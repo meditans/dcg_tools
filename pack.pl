2a92df0792b6678f1a6a93d80acef1c69285a5dd
Carlo Nucera <meditans@gmail.com>
2023-12-03 19:25:19 +0100

    First release

---
+++ pack.pl
@@ -0,0 +1,10 @@
+name(dcg_tools).
+title('Battery included bidirectional dcg parsing').
+
+version('0.0.1').
+download('https://github.com/meditans/dcg_tools/archive/v0.0.1.zip').
+
+author('Carlo Nucera','meditans@gmail.com').
+packager('Carlo Nucera','meditans@gmail.com').
+maintainer('Carlo Nucera','meditans@gmail.com').
+home('https://github.com/meditans/dcg_tools').
---
+++ prolog/dcg_tools.pl
@@ -0,0 +1,101 @@
+:- module(dcg_tools,
+         [
+             any//1,
+             nl//0,
+             space//0,
+
+             optional//1,
+             optional//2,
+             constant//1,
+             '|'//3,
+
+             uppercase//1,
+             lowercase//1,
+             letter//1,
+             word//1,
+             symbol//1,
+             digit//1,
+             natural//1,
+             integer//1,
+
+             list//1,
+             list//2,
+             list//3,
+             non_greedy//1,
+             non_greedy//2
+         ]).
+
+:- use_module(library(delay)).
+
+:- multifile delay:mode/1.
+delay:mode(lists:append(ground,_)).
+delay:mode(lists:append(_,ground)).
+delay:mode(dcg_tools:number_codes_(ground,_)).
+delay:mode(dcg_tools:number_codes_(_,ground)).
+
+
+% Higher order predicates
+optional(Dcg)      --> optional(Dcg, _).
+optional(Dcg, Res) --> call(Dcg, Res) | {Res = ``}.
+constant(A) --> seq(A).
+'|'(Dcg1, Dcg2, X) --> call(Dcg1, X) | call(Dcg2, X).
+
+
+
+seq([])     --> [].
+seq([E|Es]) --> [E], seq(Es).
+
+any(X) --> [X].
+nl     --> `\n`.
+space  --> ` `.
+
+digitCode(C)  --> [C], {code_type(C, digit)}.
+symbolCode(C) --> [C], {code_type(C, punct)}.
+lowercaseCode(C) --> [C], {code_type(C, lower)}.
+uppercaseCode(C) --> [C], {code_type(C, upper)}.
+letterCode(C) --> lowercaseCode(C) | uppercaseCode(C).
+
+lowercase(L) --> {delay(atom_codes(L, [C]))}, lowercaseCode(C).
+uppercase(L) --> {delay(atom_codes(L, [C]))}, uppercaseCode(C).
+letter(L)    --> lowercase(L) | uppercase(L).
+word(W)      --> {delay(atom_codes(W, Cs))},
+                 list(letter, Cs).
+symbol(S)    --> {delay(atom_codes(S, [C]))}, symbolCode(C).
+
+
+number_codes_(A,B) :- catch(number_codes(A,B), _, false).
+
+digit(D)   --> {delay(number_codes_(D, [C]))},
+               digitCode(C).
+natural(N) --> {delay(number_codes_(N, Ds))},
+               list(digitCode, Ds).
+integer(N) --> {delay(number_codes_(N, Codes)),
+                delay(append([Sign, Ds], Codes))},
+               optional(sign,Sign), list(digitCode, Ds).
+
+sign([0'-]) --> `-`.
+sign([0'+]) --> `+`.
+
+
+% Greedy and non greedy
+:- meta_predicate non_greedy(3,*,*).
+non_greedy(Goal) --> non_greedy(Goal,_).
+
+:- meta_predicate non_greedy(3,-,*,*).
+non_greedy(_Goal,[]) --> [].
+non_greedy(Goal,[X|Xs]) --> call(Goal,X), non_greedy(Goal,Xs).
+
+:- meta_predicate list(3,?,?,?).
+list(ElemDCG) -->
+    list(ElemDCG, _).
+
+:- meta_predicate list(3,?,?,?).
+list(ElemDCG, [Elem|Tail]) -->
+    call(ElemDCG, Elem),
+    list(ElemDCG, Tail).
+list(_, []) --> [].
+
+:- meta_predicate list(3,2,?,?,?).
+list(ElemDCG, SepDCG, [Elem|Tail]) -->
+    call(ElemDCG, Elem),
+    (call(SepDCG), list(ElemDCG, SepDCG, Tail) ; {Tail=[]}).
