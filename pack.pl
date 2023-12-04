0588fc22e812f9a17941b144bde1d2f559613080
Carlo Nucera <meditans@gmail.com>
2023-12-04 12:48:00 +0100

    new version

--- pack.pl
+++ pack.pl
@@ -1,8 +1,8 @@
 name(dcg_tools).
 title('Battery included bidirectional dcg parsing').
 
-version('0.0.1').
-download('https://github.com/meditans/dcg_tools/archive/v0.0.1.zip').
+version('0.0.2').
+download('https://github.com/meditans/dcg_tools/archive/v0.0.2.zip').
 
 author('Carlo Nucera','meditans@gmail.com').
 packager('Carlo Nucera','meditans@gmail.com').
